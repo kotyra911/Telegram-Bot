3d679b772ab55333d8eaaf5a90b27d398565dd62
root <root@debian13-bot-server>
2026-02-22 08:43:06 +0300

    some-fix on endpoint

--- app/api.py
+++ app/api.py
@@ -8,7 +8,11 @@ async def root():
     return {"status": "OK"}
 
 
-@api_router.post("/success-payment")
+@api_router.post("/success-payment-webhook")
 async def root():
+    print("Поймал вебхук от продамуса")
     return {"status": "OK"}
+
+
+
 
