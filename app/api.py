98577bc9e89b785976cb09357ebfb79a8f2cb4ab
root <root@debian13-bot-server>
2026-02-23 07:01:30 +0300

    before sync with pc

--- app/api.py
+++ app/api.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter
+import hashlib
+from fastapi import FastAPI, Request, HTTPException
+from config import PRODAMUS_KEY
 
 api_router = APIRouter()
+
+SECRET_KEY=PRODAMUS_KEY
 
 
 @api_router.get("/pulse")
@@ -9,8 +14,32 @@ async def root():
 
 
 @api_router.post("/success-payment-webhook")
-async def root():
+async def webhook_handler(request: Request):
     print("Поймал вебхук от продамуса")
+    
+    form = await request.form()
+    data = dict(form)
+
+    received_signature = data.pop("signature", None)
+
+    if not received_signature:
+        raise HTTPException(status_code=400, detail="No signature")
+
+    # Сортируем параметры
+    sorted_items = sorted(data.items())
+
+    # Формируем строку
+    sign_string = "".join(str(value) for _, value in sorted_items)
+    sign_string += SECRET_KEY
+
+    # Считаем хеш
+    calculated_signature = hashlib.sha256(
+        sign_string.encode()
+    ).hexdigest()
+
+    if calculated_signature != received_signature:
+        raise HTTPException(status_code=400, detail="Invalid signature")
+
     return {"status": "OK"}
 
 
